c61f6dd2765fdf249eea25aff92a19d7c22abac2
Jan Vansteenkiste <jan@vstone.eu>
2013-05-22 09:52:59 +0200

    If a datadir has been setup with a environment in it, sync hieradata to the environment folder

--- scripts/puppetapply.sh
+++ scripts/puppetapply.sh
@@ -4,28 +4,31 @@ environment=${1-production}
 boxname=${2-unknown}
 sources=${3-puppet}
 
-## Make sure the required directories exist.
+## Puppet setup.
 [ -d /etc/puppet/environments/${environment} ] || mkdir -pv /etc/puppet/environments/${environment}
-[ -d /etc/puppet/hieradata ] || mkdir -pv /etc/puppet/hieradata
 [ -d /vagrant/graphs/${boxname} ] || mkdir -pv /vagrant/graphs/${boxname}
-
-## Copy general config files.
-[ -f /vagrant/${sources}/hiera.yaml ] && cp -v /vagrant/${sources}/hiera.yaml /etc/puppet/hiera.yaml  || \
-  ( [ -f /vagrant/${sources}/hiera.yaml-base ] && cp -v /vagrant/${sources}/hiera.yaml-base /etc/puppet/hiera.yaml )
 [ -f /vagrant/${sources}/puppet.conf-base ] && cp -v /vagrant/${sources}/puppet.conf-base /etc/puppet/puppet.conf
-
-## Sync puppet code to the right environment.
+# Sync puppet code to the right environment.
 rsync -alrcWt --del --progress \
   --exclude=.git --exclude=.svn --exclude=*.swp \
   --exclude=vendor/ --exclude=.vendor/ \
   /vagrant/${sources}/* /etc/puppet/environments/${environment}/
 
+## Hiera setup
+[ -f /vagrant/${sources}/hiera.yaml ] && cp -v /vagrant/${sources}/hiera.yaml /etc/puppet/hiera.yaml  || \
+  ( [ -f /vagrant/${sources}/hiera.yaml-base ] && cp -v /vagrant/${sources}/hiera.yaml-base /etc/puppet/hiera.yaml )
+
+hiera_sync_to=/etc/puppet/hieradata
+## If we include the environment in the datadir setting, add it to the path.
+#  Only works where the exact path is /etc/puppet/hieradata/environment ofcourse.
+[ -f /etc/puppet/hiera.yaml ] && grep -q ':datadir:.*%{environment}' /etc/puppet/hiera.yaml && hiera_sync_to=/etc/puppet/hieradata/${environment}
+[ -d $hiera_sync_to ] || mkdir -pv $hiera_sync_to
 
 ## Sync hieradata code.
 if [ -d /vagrant/hieradata ]; then
   rsync -alrcWt --del --progress \
     --exclude=.git --exclude=.svn \
-    /vagrant/hieradata/ /etc/puppet/hieradata/
+    /vagrant/hieradata/ $hiera_sync_to
 fi;
 
 exec puppet apply \
